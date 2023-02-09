4d2f4f775c82663cf07b85e1d654a5bc3d5cab84
rkr-abs <rkr@abstractit.in>
2023-02-09 14:33:10 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import { React } from 'react';
+import { peek } from '@laufire/utils/debug';
+import { React, useEffect } from 'react';
 import './App.scss';
 
-const App = () =>
-	<div className="App">
-		Ready to start.
+const notify = () => {
+	const options = {
+		body: 'Hi',
+
+	};
+
+	Notification.permission === 'granted'
+		? peek(new Notification('title', options))
+		: Notification.requestPermission();
+
+	navigator.mediaDevices
+		.getUserMedia({ audio: true, video: false })
+		.then((stream) => {
+			window.localStream = stream;
+			window.localAudio.srcObject = stream;
+			window.localAudio.autoplay = true;
+		});
+};
+const App = () => {
+	useEffect(notify, []);
+	return <div className="App">
+		<button onClick={ notify }>Click</button>
 	</div>;
+};
 
 export default App;
