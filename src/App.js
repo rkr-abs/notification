e3723a1ecc74ea81362128478484510df692153b
rkr-abs <rkr@abstractit.in>
2023-03-14 17:07:26 +0530

    Testing

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import { React } from 'react';
 import './App.scss';
 import PermissionStore from './services/PermissionsStore';
 
-const App = () => <div className="App">
-	<button onClick={ async () => {
-		await PermissionStore({ data: {}, pipe: peek })({
-			action: 'read',
-			entity: 'permissions', data: { },
-		});
-	} }
-	>Click</button>
-	<img src="https://www.mordeo.org/files/uploads/2020/09/Iron-Man-Fortnite-4K-Ultra-HD-Mobile-Wallpaper-950x1689.jpg" alt="no-lll"/>
-	<h1>HELLo</h1>
-</div>;
+const App = () => {
+	peek('HELLO Nirupan :)');
+	return <div className="App">
+		<button onClick={ async () => {
+			await PermissionStore({ data: {}, pipe: peek })({
+				action: 'read',
+				entity: 'permissions', data: {},
+			});
+		} }
+		>Click</button>
+		<img src="https://www.mordeo.org/files/uploads/2020/09/Iron-Man-Fortnite-4K-Ultra-HD-Mobile-Wallpaper-950x1689.jpg" alt="no-lll"/>
+		<h1>HELLo</h1>
+	</div>;
+};
 
 export default App;
