9f4e43313c1b5b39c1cc66a4fbf0eedf3f0880d1
rkr-abs <rkr@abstractit.in>
2023-03-23 12:11:48 +0530

    Testing contacts permission in webview

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 /* eslint-disable max-lines-per-function */
 /* eslint-disable max-len */
+import { peek } from '@laufire/utils/debug';
+import { values } from '@laufire/utils/lib';
 import { React, useState } from 'react';
 import './App.scss';
 import PermissionStore from './services/PermissionsStore';
 
-const App = () => {
-	const [state, setState] = useState([]);
+const App = (context) => {
+	const { config: { permissions, requestPermissions }} = context;
+	const [state, setState] = useState({});
 
-	const pipe = (context) => {
-		setState(() => context.data);
+	const pipe = (data) => {
+		peek(data);
+		setState(() => data.data);
 
-		return alert(context.status);
+		return alert(data.status);
 	};
 
 	return <div className="App">
@@ -18,18 +22,35 @@ const App = () => {
 			src="https://www.mordeo.org/files/uploads/2020/09/Iron-Man-Fortnite-4K-Ultra-HD-Mobile-Wallpaper-950x1689.jpg"
 			alt="no-lll"
 			height="250px"
-		/>
-		<h1>HELLO</h1>
-		<button onClick={ async () => {
-			await PermissionStore({ data: {}, pipe: pipe })({
-				action: 'read',
-				entity: 'permissions', data: { },
-			});
-		} }
-		>RequestPermissions</button>
-		<div>{
-			state.map((e, key) => <h1 key={ key }>{e.id} : {e.status}</h1>)
-		}</div>
+		/><h1>ReadPermissions</h1>
+		{permissions.map((permission, key) =>
+			<span key={ key }>
+				<button
+					style={ { margin: '10px' } }
+					onClick={ async () => {
+						await PermissionStore({ data: {}, pipe: pipe })({
+							action: 'read',
+							entity: 'permissions', data: { id: permission },
+						});
+					} }
+				>{permission}</button>
+			</span>)}<hr/>
+		<h1>RequestPermissions</h1>
+		{requestPermissions.map((permission, key) =>
+			<span key={ key }>
+				<button
+					style={ { margin: '10px' } }
+					onClick={ async () => {
+						await PermissionStore({ data: {}, pipe: pipe })({
+							action: 'update',
+							entity: 'permissions', data: { id: permission },
+						});
+					} }
+				>{permission}
+				</button></span>)}
+		<div>a{navigator.userAgent}</div>
+		<button onClick={ () => navigator.contacts.getProperties() }> Contact</button>
+		<div>{values(state).map((e, key) => <h1 key={ key }>{`${ e }`}</h1>)}</div>
 	</div>;
 };
 
